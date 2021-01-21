a23ed6b56262ce2e2da3cb5d3169d04995df4e23
homsau <james.p.cowart@gmail.com>
2021-01-20 21:49:36 -0500

    Add project files.

---
+++ FirstConsoleApplication/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FirstConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int first, second, result;
+            Console.Write("First number: ");
+            first = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Second number: ");
+            second = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("1 = + Add,\n2 = - Subtract,\n3 = * Multiply");
+            
+            int operation = Convert.ToInt32(Console.ReadLine());
+            if (operation == 1) {
+                result = first + second;
+                Console.WriteLine(result);
+            } else if (operation == 2) {
+                result = first - second;
+                Console.WriteLine(result);
+            } else if (operation == 3) {
+                result = first * second;
+                Console.WriteLine(result);
+            } else {
+                Console.WriteLine("try again");
+            }
+        }
+    }
+}
